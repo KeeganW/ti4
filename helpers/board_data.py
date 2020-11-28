4d071dfb007b234ce1ac4f2f3436d390f12084c7
KeeganW <keeganw@andrew.cmu.edu>
2020-11-28 01:07:29 -0500

    feat(map): additional map layouts

--- helpers/board_data.py
+++ helpers/board_data.py
@@ -44,6 +44,7 @@ The numbers in each list reference a standard TI hex layout labeled board:
                \____/  28  \____/
                     \      /
                      \____/
+[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
 """
 boards = {
     2: {
@@ -61,6 +62,15 @@ boards = {
         'normal': {
             'description': 'Control what is around you for resources, and attack the center for '
                            'victory points.',
+            'empty_tiles': [19, 20, 24, 25, 26, 30, 31, 32, 36],
+            'home_worlds': [22, 28, 34],
+            'primary_tiles': [9, 13, 17, 21, 23, 27, 29, 33, 35],
+            'secondary_tiles': [7, 8, 10, 11, 12, 14, 15, 16, 18],
+            'tertiary_tiles': [1, 2, 3, 4, 5, 6]
+        },
+        'compact': {
+            'description': 'A more compact variant, that encourages fighting. Recommended to play '
+                           'with high resource values.',
             'empty_tiles': [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
             'home_worlds': [10, 14, 18],
             'primary_tiles': [7, 9, 11, 13, 15, 17],
@@ -80,11 +90,36 @@ boards = {
         'normal': {
             'description': 'Control what is around you for resources, and attack the center for '
                            'victory points.',
+            'empty_tiles': [],
+            'home_worlds': [23, 27, 32, 36],
+            'primary_tiles': [19, 22, 24, 26, 28, 31, 33, 35],
+            'secondary_tiles': [7, 9, 10, 12, 13, 15, 16, 18],
+            'tertiary_tiles': [1, 2, 3, 4, 5, 6, 8, 11, 14, 17, 20, 21, 25, 29, 30, 34]
+        },
+        'horizontal': {
+            'description': 'A slightly smaller than normal 4 player board which is aligned '
+                           'horizontally.',
             'empty_tiles': [19, 20, 27, 28, 29, 36],
             'home_worlds': [22, 25, 31, 34],
             'primary_tiles': [9, 11, 15, 17],
             'secondary_tiles': [21, 23, 24, 26, 30, 32, 33, 35],
             'tertiary_tiles': [1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 13, 14, 16, 18, 22, 25, 31, 34]
+        },
+        'vertical': {
+            'description': 'An even smaller board aligned vertically.',
+            'empty_tiles': [19, 22, 23, 24, 25, 31, 32, 33, 34],
+            'home_worlds': [21, 26, 30, 35],
+            'primary_tiles': [9, 11, 15, 17, 20, 27, 29, 36],
+            'secondary_tiles': [7, 8, 12, 13, 14, 18],
+            'tertiary_tiles': [1, 2, 3, 4, 5, 6, 10, 16]
+        },
+        'gaps': {
+            'description': 'Vertical, but with gaps to encourage fights with neighbors.',
+            'empty_tiles': [1, 4, 19, 22, 23, 24, 25, 31, 32, 33, 34],
+            'home_worlds': [21, 26, 30, 35],
+            'primary_tiles': [9, 11, 15, 17, 20, 27, 29, 36],
+            'secondary_tiles': [7, 8, 12, 13, 14, 18],
+            'tertiary_tiles': [2, 3, 5, 6, 10, 16]
         }
     },
     5: {
@@ -96,7 +131,16 @@ boards = {
             'primary_tiles': [9, 11, 13, 15, 17],
             'secondary_tiles': [8, 18, 22, 24, 26, 27, 29, 30, 32, 34],
             'tertiary_tiles': [1, 2, 3, 4, 5, 6, 7, 10, 12, 14, 16, 19, 20, 23, 33, 36]
-        }
+        },
+        'diamond': {
+            'description': 'A slightly smaller map than normal, which focuses on being closer to '
+                           'Mecatol Rex.',
+            'empty_tiles': [22, 25, 26, 30, 31, 34],
+            'home_worlds': [21, 24, 28, 32, 35],
+            'primary_tiles': [9, 11, 13, 15, 17],
+            'secondary_tiles': [8, 10, 16, 18, 20, 23, 27, 29, 33, 36],
+            'tertiary_tiles': [1, 2, 3, 4, 5, 6, 7, 12, 14, 19]
+        },
     },
     6: {
         'normal': {
