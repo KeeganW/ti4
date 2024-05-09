d5e391256875e8b5116a64dced002e355f474d3c
jumbocarrot0 <jumbocarrot0@gmail.com>
2024-05-09 18:13:24 +1000

    Updated bootstrap version Removed unecessary import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.js";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
